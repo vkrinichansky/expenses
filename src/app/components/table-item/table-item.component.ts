2eb08acc3a00446804e1c963685d0e2dc3c9e289
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2022-02-20 11:23:39 +0200

    remove controls

--- src/app/components/table-item/table-item.component.ts
+++ src/app/components/table-item/table-item.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AppDataService } from '../../services/app-data/app-data.service';
+import { Component, Input } from '@angular/core';
 import { TablesTypesEnum } from '../../consts';
 import { TableItem } from '../../types';
 
@@ -8,16 +7,7 @@ import { TableItem } from '../../types';
   templateUrl: './table-item.component.html',
   styleUrls: ['./table-item.component.scss'],
 })
-export class TableItemComponent implements OnInit {
+export class TableItemComponent {
   @Input() item: TableItem;
   @Input() table: TablesTypesEnum;
-  @Input() displayControls = true;
-
-  constructor(private appDataService: AppDataService) {}
-
-  ngOnInit(): void {}
-
-  removeItem(): void {
-    this.appDataService.removeCategory(this.item.name, this.table);
-  }
 }
