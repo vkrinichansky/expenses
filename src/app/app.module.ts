4202248d011bcbaa601cd393192f7191812d223f
Vadim Krinichansky <vadim.krinichansky@outlook.com>
2022-04-30 16:03:18 +0300

    removed table item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
 import { AddCategoryComponent } from './components/add-category/add-category.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TableItemComponent } from './components/table/table-item/table-item.component';
 import { CurrentBalanceComponent } from './components/current-balance/current-balance.component';
 import { AddFormComponent } from './components/add-form/add-form.component';
 import { DailyHistoryComponent } from './components/daily-history/daily-history.component';
@@ -27,7 +25,6 @@ import { StateService } from './services/state-service/state.service';
     AppComponent,
     TableComponent,
     AddCategoryComponent,
-    TableItemComponent,
     CurrentBalanceComponent,
     AddFormComponent,
     DailyHistoryComponent,
